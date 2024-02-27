5639f6cd28f25a79238c5fb19688562f23327475
entornoInteractivo <cristianvelasco1529@gmail.com>
2024-02-27 17:14:40 -0500

    with cache dependencies test caching 16

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
     baseUrl: 'https://gradi-theme-base.myshopify.com',
     setupNodeEvents (on, config) {
       // implement node event listeners here
-    }
+    },
+    excludeSpecPattern: 'cypress/e2e/side_cart.cy.js'
   },
   // retries: 2
 })
