44ed3d898826cf636ef0167aac04ed492ff03ccd
entornoInteractivo <cristianvelasco1529@gmail.com>
2024-02-29 14:28:21 -0500

    Native lighthouse CI: test 5

--- setPreviewCookies.js
+++ setPreviewCookies.js
@@ -8,6 +8,7 @@ const env = {
 module.exports = async (browser) => {
   // launch browser for LHCI
   console.error('Getting a new page...');
+  console.log(browser);
   const page = await browser.newPage();
   // Get password cookie if password is set
   
