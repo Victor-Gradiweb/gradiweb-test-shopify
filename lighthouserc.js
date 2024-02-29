90a743e255e7daf8756aace47d80505fca2f5516
entornoInteractivo <cristianvelasco1529@gmail.com>
2024-02-29 12:23:23 -0500

    Native lighthouse CI

---
+++ lighthouserc.js
@@ -0,0 +1,38 @@
+const env = {
+  host: 'https://gradi-theme-base.myshopify.com',
+  queryString: '?preview_theme_id=134439108788&_fd=0&pb=0',
+  password: 'gradiweb',
+  previewUrl: 'https://gradi-theme-base.myshopify.com?preview_theme_id=134439108788',
+  collectionUrl: 'collections/all',
+  productUrl: 'products/ricoh-theta-sc2'
+}
+
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        `${env.previewUrl}`,
+        `${env.host}${env.collectionUrl}${env.queryString}`,
+        `${env.host}${env.productUrl}${env.queryString}`
+      ],
+      puppeteerScript: './setPreviewCookies.js',
+      puppeteerLaunchOptions: {
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--disable-gpu"
+        ]
+      }
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", {"minScore": 0.6}],
+        "categories:accessibility": ["warn", {"minScore": 0.8}]
+      }
+    }
+  },
+};
---
+++ setPreviewCookies.js
@@ -0,0 +1,30 @@
+const env = {
+  host: 'https://gradi-theme-base.myshopify.com',
+  queryString: '?preview_theme_id=134439108788&_fd=0&pb=0',
+  password: 'gradiweb',
+  previewUrl: 'https://gradi-theme-base.myshopify.com?preview_theme_id=134439108788'
+}
+
+module.exports = async (browser) => {
+  // launch browser for LHCI
+  console.error('Getting a new page...');
+  const page = await browser.newPage();
+  // Get password cookie if password is set
+  
+  if (env.password !== '') {
+    console.error('Getting password cookie...');
+    await page.goto(`${env.host}/password${env.queryString}`);
+    await page.waitForSelector('form[action*=password] input[type="password"]');
+    await page.$eval('form[action*=password] input[type="password"]', input => input.value = env.password);
+    await Promise.all([
+      page.waitForNavigation(),
+      page.$eval('form[action*=password]', form => form.submit()),
+    ])
+  }
+  
+  // Get preview cookie
+  console.error('Getting preview cookie...');
+  await page.goto(env.previewUrl);
+  // close session for next run
+  await page.close();
+};
