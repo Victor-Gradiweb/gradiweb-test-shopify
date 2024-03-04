d2342cd3ad1f85ad2d9d87ba0918b9f97d4986b5
crisdeve <cristian@gradiweb.com>
2024-03-04 16:10:23 -0500

    pass config lhci config theme_id testing 15

--- setPreviewCookies.js
+++ setPreviewCookies.js
@@ -1,20 +1,17 @@
-const process = require('process')
 let counter = 1
 
 module.exports = async (browser, context) => {
-  const password = process.env.SHOP_PASSWORD
-
   console.log({ context })
   // launch browser for LHCI
   console.error('Getting a new page...')
   const page = await browser.newPage()
 
   // Get password cookie if password is set
-  if (`${password}` !== '' && counter === 1) {
+  if (`${context.shopPassword}` !== '' && counter === 1) {
     console.error('Getting password page cookie...')
     await page.goto(context.url)
     await page.waitForSelector('form[action*=password] input[type="password"]')
-    await page.$eval('form[action*=password] input[type="password"]', (input) => { input.value = `${password}` })
+    await page.$eval('form[action*=password] input[type="password"]', (input) => { input.value = `${context.shopPassword}` })
     await Promise.all([
       page.waitForNavigation(),
       page.$eval('form[action*=password]', form => form.submit())
