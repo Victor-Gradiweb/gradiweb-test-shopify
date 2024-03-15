fe9893a46db9be82e2297381201a60d7bc15f645
Michael Rojas <msrojas265@gmail.com>
2024-03-14 21:12:32 -0500

    jsDoc class ProductPage

--- cypress/class/ProductPage.js
+++ cypress/class/ProductPage.js
@@ -1,14 +1,25 @@
 import { sideCart } from './SideCart'
 
+/**
+ * Represents a product page.
+ */
 export class ProductPage {
+  /**
+   * Constructs a ProductPage object.
+   */
   constructor () {
+    // Selectors for the product page and side cart.
     this.productPageSelector = Cypress.env('product_page')
     this.sideCartSelector = Cypress.env('sidecart')
   }
 
+  /**
+   * Checks if the breadcrumbs are correctly displayed.
+   */
   checkBreadcrumb () {
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
+        // Iterate through each breadcrumb link.
         cy.get(this.productPageSelector.section_product)
           .find(this.productPageSelector.breadcrumbs)
           .as('breadcrumbs')
@@ -19,6 +30,7 @@ export class ProductPage {
             }
           })
 
+        // Verify breadcrumb text matches product title.
         cy.get('h1').should('exist').and('be.visible').invoke('text').then(productTitle => {
           this.verifyBreadcrumbText(productTitle)
         })
@@ -26,6 +38,10 @@ export class ProductPage {
     })
   }
 
+  /**
+   * Verifies if the breadcrumb text matches the product title.
+   * @param {string} productTitle - The title of the product.
+   */
   verifyBreadcrumbText (productTitle) {
     cy.get('@breadcrumbs').eq(2).then(($breadcrumb) => {
       const breadcrumbText = $breadcrumb.text().trim()
@@ -34,30 +50,49 @@ export class ProductPage {
     })
   }
 
+  /**
+   * Checks if the product page is visible.
+   * @returns {Promise<boolean>} A promise resolving to true if the product page is visible, otherwise false.
+   */
   isProductPageVisible () {
     return cy.get('body').then((body) => {
       return body.find(this.productPageSelector.section_product).length > 0
     })
   }
 
+  /**
+   * Clicks on the arrow element specified by the selector.
+   * @param {string} selector - The selector for the arrow element.
+   */
   clickArrow (selector) {
     cy.get(this.productPageSelector.section_product)
       .find(selector)
       .click()
   }
 
+  /**
+   * Verifies if the next arrow for product media is enabled.
+   */
   verifyNextArrowEnables () {
     cy.get(this.productPageSelector.section_product)
       .find(this.productPageSelector.arrow_next_product_media)
       .should('be.enabled')
   }
 
+  /**
+ * Verify if the previous arrow for navigating through product media is disabled.
+ * @returns {void}
+ */
   verifyPrevArrowDisables () {
     cy.get(this.productPageSelector.section_product)
       .find(this.productPageSelector.arrow_prev_product_media)
       .should('not.be.enabled')
   }
 
+  /**
+ * Check if product image exists on the product page.
+ * @returns {void}
+ */
   checkProductImage () {
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
@@ -68,6 +103,10 @@ export class ProductPage {
     })
   }
 
+  /**
+ * Clicks on the next arrow for upsell products and verifies if it enables the next arrow.
+ * @returns {void}
+ */
   clickNextUpsell () {
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
@@ -77,6 +116,10 @@ export class ProductPage {
     })
   }
 
+  /**
+ * Clicks on the previous arrow for upsell products and verifies if it disables the previous arrow.
+ * @returns {void}
+ */
   clickPrevtUpsell () {
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
@@ -86,6 +129,10 @@ export class ProductPage {
     })
   }
 
+  /**
+ * Verify if the image changes upon clicking the navigation arrow.
+ * @returns {void}
+ */
   verifyImageChangeOnArrowClick () {
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
@@ -103,6 +150,10 @@ export class ProductPage {
     })
   }
 
+  /**
+ * Clicks on the next arrow until it becomes disabled.
+ * @returns {void}
+ */
   clickNextUntilDisabled () {
     const clickNext = () => {
       cy.get(this.productPageSelector.arrow_next_product_media).then($button => {
@@ -116,6 +167,10 @@ export class ProductPage {
     cy.get(this.productPageSelector.arrow_next_product_media).should('be.disabled')
   }
 
+  /**
+ * Check if the product title exists and is visible on the product page.
+ * @returns {void}
+ */
   checkProductTitle () {
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
@@ -124,19 +179,29 @@ export class ProductPage {
     })
   }
 
+  /**
+   * Adds the currently displayed product to the cart.
+   * @returns {void}
+   */
   addToCartProductPage () {
     let productTitle
+
+    // Check if product page is visible
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
+      // Get product title
         cy.get('h1').should('exist').and('be.visible').invoke('text').then((title) => {
           productTitle = title.trim()
 
+          // Find and click add to cart button
           cy.get(this.productPageSelector.section_product)
             .find(this.productPageSelector.add_product_product_page)
             .then((addProduct) => {
               if (!addProduct.prop('disabled')) {
                 addProduct.click()
                 sideCart.checkIfSideCartIsActive()
+
+                // Verify product added to cart
                 cy.get(this.sideCartSelector.cart_item)
                   .should('have.length', 1)
                   .and('contain', productTitle)
@@ -149,37 +214,67 @@ export class ProductPage {
     })
   }
 
+  /**
+ * Adds multiple quantities of the product to the cart.
+ * @returns {void}
+ */
   addMultipleQuantityToCartProductPage () {
     let initialQuantity = 0
 
+    // Check if product page is visible
     this.isProductPageVisible().then((isVisible) => {
       if (isVisible) {
+      // Get initial quantity
         this.getInitialQuantity().then((value) => {
           initialQuantity = parseInt(value)
+
+          // Increment quantity and add to cart
           this.incrementQuantityAndAddToCart()
+
+          // Verify cart quantity
           this.verifyCartQuantity(initialQuantity)
         })
       }
     })
   }
 
+  /**
+ * Retrieves the initial quantity of the product.
+ * @returns {Promise<string>} The initial quantity as a string.
+ */
   getInitialQuantity () {
     return cy.get(this.productPageSelector.input_quantity_from_productPage)
       .invoke('attr', 'value')
   }
 
+  /**
+ * Increments quantity and adds the product to the cart.
+ * @returns {void}
+ */
   incrementQuantityAndAddToCart () {
+  // Click add quantity button
     cy.get(this.productPageSelector.add_quantity_from_productPage).click()
+
+    // Click add to cart button
     cy.get(this.productPageSelector.add_product_product_page).click()
+
+    // Check if side cart is active
     sideCart.checkIfSideCartIsActive()
   }
 
+  /**
+ * Verifies the quantity in the cart after adding the product.
+ * @param {number} initialQuantity - The initial quantity of the product.
+ * @returns {void}
+ */
   verifyCartQuantity (initialQuantity) {
     cy.wrap(null).then(() => {
+    // Check if cart quantity is updated
       cy.get(this.sideCartSelector.input_quantity)
         .should('have.value', (initialQuantity + 1).toString())
     })
   }
 }
 
+// Export an instance of ProductPage
 export const productPage = new ProductPage()
