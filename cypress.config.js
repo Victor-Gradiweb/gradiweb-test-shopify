0292e0ebd0c2113c8607e40189567d9c8056bf45
crisdeve <cristian@gradiweb.com>
2024-04-11 22:56:32 -0500

    merge survivor ci

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  env:{
+  env: {
     // "BASE_URL": "https://gradi-theme-base.myshopify.com/",
-    "PREVIEW_THEME": "135179665588",
-    "PASSWORD_STORE": "gradiweb",
-    "PATH_COLLECTION": "all",
-    "HANDLE_PRODUCT": "ricoh-theta-sc2"
+    PREVIEW_THEME: '',
+    PASSWORD_STORE: '',
+    PATH_COLLECTION: '',
+    HANDLE_PRODUCT: ''
   },
   e2e: {
-    baseUrl: "https://gradi-theme-base.myshopify.com", 
-    screenshotsFolder: false,
+    baseUrl: 'https://gradi-theme-base.myshopify.com',
+    screenshotsFolder: false
     /* excludeSpecPattern: 'cypress/e2e/side_cart.cy.js' */
   },
-  retries: 1,
-});
+  retries: 1
+})
