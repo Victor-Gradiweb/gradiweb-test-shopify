7eb3aebc88ba8744d9c177f75e6b9fbf066bb54c
Code Rada <msrojas265@gmail.com>
2024-04-18 08:15:09 -0500

    add library terminal cypress report

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,12 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: 'https://gradi-theme-base.myshopify.com',
-    screenshotsFolder: false
+    screenshotsFolder: false,
+    setupNodeEvents(on, config) {
+      require('cypress-terminal-report/src/installLogsPrinter')(on, {
+        defaultTrimLength: 1000,
+      });
+    }
     /* excludeSpecPattern: 'cypress/e2e/side_cart.cy.js' */
   },
   // retries: 1,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,3 @@
 const commandsContext = require.context('../commands/', false, /\.js$/);
 commandsContext.keys().forEach(commandsContext);
+require('cypress-terminal-report/src/installLogsCollector')();
