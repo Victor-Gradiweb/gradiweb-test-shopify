082461b866d18b68692679d30464fe1f7f89b5f8
Michael Rojas <msrojas265@gmail.com>
2024-04-30 14:54:08 -0500

    add const object designSystem

--- cypress/e2e/sections/example-desing-system-section.cy.js
+++ cypress/e2e/sections/example-desing-system-section.cy.js
@@ -1,27 +1,25 @@
 import { visitAndBreakPassword } from "../../commands/visit-web-site"
 import designSystem from '../.env/design-system.json'
 
-const { sections } = designSystem;
-const sectionEntries = Object.entries(sections);
+const { sections: { dual_section, footer, call_to_action }} = designSystem
 
 describe('E-commerce Testing: Dual section', () => {
     beforeEach(() => {
         visitAndBreakPassword()
     })
-    sectionEntries.forEach(([sectionName, sectionSelector]) => {
-    it('Verify headdings', () => { cy.verifyHeadings(sections[dual_section], 'h1, h2, h3, h4, h5, h6') })
-    it('Verify headdings Exception', () => { cy.verifyHeadings(sections[footer], 'h2', { size: 18 }) })
-    it('Verify button large', () => { cy.verifyButtons(sections[call_to_action], 'large') })
-    it('Verify button meidum', () => { cy.verifyButtons(sections[call_to_action], 'medium') })
-    // it('Verify button small', () => { cy.verifyButtons(sections[call_to_action], 'small') }) // error empty data
+
+    it('Verify headdings', () => { cy.verifyHeadings(dual_section, 'h1, h2, h3, h4, h5, h6') })
+    it('Verify headdings Exception', () => { cy.verifyHeadings(footer, 'h2', { size: 18 }) })
+    it('Verify button large', () => { cy.verifyButtons(call_to_action, 'large') })
+    it('Verify button meidum', () => { cy.verifyButtons(call_to_action, 'medium') })
+    // it('Verify button small', () => { cy.verifyButtons(call_to_action, 'small') }) // error empty data
     it('Verify p and span text styles', () => { cy.paragraphText(dual_section) })
     it('Verify text p and span styles per object', () => { 
-        cy.paragraphText(sections[dual_section], {
+        cy.paragraphText(dual_section, {
           fontSize: {
             p: 16, // Custom font size for <p>
             span: 14 // Custom font size for <span>
           }
         })
       })
-    })
 })
