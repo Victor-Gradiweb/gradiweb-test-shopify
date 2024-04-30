9cab49fe9688fcbea46d7a1f943b51128ebde227
Michael Rojas <msrojas265@gmail.com>
2024-04-30 14:39:47 -0500

    add sections object

--- cypress/e2e/sections/example-desing-system-section.cy.js
+++ cypress/e2e/sections/example-desing-system-section.cy.js
@@ -1,23 +1,27 @@
 import { visitAndBreakPassword } from "../../commands/visit-web-site"
 import designSystem from '../.env/design-system.json'
 
+const { sections } = designSystem;
+const sectionEntries = Object.entries(sections);
+
 describe('E-commerce Testing: Dual section', () => {
     beforeEach(() => {
         visitAndBreakPassword()
     })
-
-    it('Verify headdings', () => { cy.verifyHeadings(designSystem.sections.dual_section, 'h1, h2, h3, h4, h5, h6') })
-    it('Verify headdings Exception', () => { cy.verifyHeadings(designSystem.sections.footer, 'h2', { size: 18 }) })
-    it('Verify button large', () => { cy.verifyButtons(designSystem.sections.call_to_action, 'large') })
-    it('Verify button meidum', () => { cy.verifyButtons(designSystem.sections.call_to_action, 'medium') })
-    // it('Verify button small', () => { cy.verifyButtons(designSystem.sections.call_to_action, 'small') }) // error empty data
-    it('Verify p and span text styles', () => { cy.paragraphText(designSystem.sections.dual_section) })
+    sectionEntries.forEach(([sectionName, sectionSelector]) => {
+    it('Verify headdings', () => { cy.verifyHeadings(sections[dual_section], 'h1, h2, h3, h4, h5, h6') })
+    it('Verify headdings Exception', () => { cy.verifyHeadings(sections[footer], 'h2', { size: 18 }) })
+    it('Verify button large', () => { cy.verifyButtons(sections[call_to_action], 'large') })
+    it('Verify button meidum', () => { cy.verifyButtons(sections[call_to_action], 'medium') })
+    // it('Verify button small', () => { cy.verifyButtons(sections[call_to_action], 'small') }) // error empty data
+    it('Verify p and span text styles', () => { cy.paragraphText(dual_section) })
     it('Verify text p and span styles per object', () => { 
-        cy.paragraphText(designSystem.sections.dual_section, {
+        cy.paragraphText(sections[dual_section], {
           fontSize: {
             p: 16, // Custom font size for <p>
             span: 14 // Custom font size for <span>
           }
         })
       })
+    })
 })
