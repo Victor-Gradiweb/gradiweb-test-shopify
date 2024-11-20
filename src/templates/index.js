30a2e7d74132ec5947359b1ef09ba06f94c789e8
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2024-11-19 22:55:38 -0500

    filter

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,10 +4,11 @@ import { loadSlider } from '../components/slider-component'
 import { headerFooterDefer } from '../components/header-footer-defer'
 import { sectionsLoadDefer } from '../components/sections-defer'
 import initWindows from '../components/window'
-
+import filterBooks from '../components/filter-books'
 headerFooterDefer()
 loadSlider()
 sectionsLoadDefer()
+filterBooks()
 
 initWindows({
   control: '#burger-nav-js',
